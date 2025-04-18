686df8f7a5cb8795f883cf816423b33355f7d738
giaccneto <giaccento@hotmail.com>
2025-04-18 08:49:33 -0400

    Implementada toda tarefa de notificação por email

---
+++ src/main/java/com/giaccneto/notificacao/infraestructure/exceptions/EmailException.java
@@ -0,0 +1,11 @@
+package com.giaccneto.notificacao.infraestructure.exceptions;
+
+public class EmailException extends RuntimeException{
+
+    public EmailException(String mensagem){
+        super(mensagem);
+    }
+    public EmailException(String mensagem, Throwable throwable){
+        super(mensagem, throwable);
+    }
+}
